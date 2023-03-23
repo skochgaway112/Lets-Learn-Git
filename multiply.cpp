4dff199e8a5112fb185b25c6114e99c320552444
Shreshtha Kochgaway <skochgaway112@gmail.com>
2023-03-23 22:24:05 +0530

    multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=5;
+    int b=6;
+    int multiply=0;
+    multiply=a*b;
+    cout<<multiply<<endl;
+    cout<<"the final multiplication is as the answer given below";
+    return 0;
+}
